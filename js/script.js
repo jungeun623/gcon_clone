8798175b03a2597b8286ab76eaf4b5d6261017eb
LEEJUNGUEN <jungeun623@gmail.com>
2024-08-27 16:12:10 +0900

    스크립트 수정

--- js/script.js
+++ js/script.js
@@ -62,34 +62,36 @@ $(document).ready(function () {
     });
   });
   // header스크롤 적용
-  const hTop = $(".header-top");
-  const hTop_H = hTop.height();
-  const hMiddle = $(".header-middle");
-  const hMiddle_H = hMiddle.height();
-  console.log(hMiddle_H);
-  // console.log(hTop_H);
-  const hHeight = hTop_H + hMiddle_H;
-  $(window).scroll(function () {
+  const hTop = $(".header-top")
+  const hTop_H = hTop.height()
+  const hMiddle = $(".header-middle")
+  const hMiddle_H = hMiddle.height()
+  // console.log(hMiddle_H);
+  
+  // console.log( hTop_H );
+  const hHeight = hTop_H + hMiddle_H
+  $(window).scroll(function(){
     // 스크롤바의 위치값을 파악한다
-    const scY = $(window).scrollTop();
+    const scY = $(window).scrollTop()
     // console.log(scY);
-    if (scY >= hHeight) {
-      $(".header").addClass("h-fix");
-      $(".logo-gnb").addClass("h-show");
-      $(".gnb").addClass("h-fix-gnb");
-    } else {
-      $(".header").removeClass("h-fix");
-      $(".logo-gnb").removeClass("h-show");
-      $(".gnb").removeClass("h-fix-gnb");
+    if(scY >= hHeight){
+      $(".header").addClass("h-fix")
+      $(".logo-gnb").addClass("h-show")
+      $(".gnb").addClass("h-fix-gnb")
+    }else{
+      $(".header").removeClass("h-fix")
+      $(".logo-gnb").removeClass("h-show")
+      $(".gnb").removeClass("h-fix-gnb")
     }
-  });
-  // swiper
+    
+  })
+  // swiper 
   // content 슬라이드
-  // const sw_content = new Swiper(".sw-content", {
+  // const sw_content = new Swiper(".sw-content",{
 
   // })
   // sw-notice 슬라이드
-  const sw_notice = new Swiper(".sw-notice", {
+  const sw_notice = new Swiper(".sw-notice",{
     autoplay: {
       delay: 2000,
       disableOnInteraction: false,
@@ -104,14 +106,15 @@ $(document).ready(function () {
       el: ".sw-notice-pg",
       type: "fraction",
     },
-  });
-  // 일시멈춤
-  $(".sw-notice-pause").click(function(){
-    // 스와이퍼가 중첩되어져서 처리를 함
-    for(let i = 0; i < sw_notice.length ; i++){
-      sw_notice[i].autoplay.stop()
-    }
   })
-  // 자동재생 실행
+  $(".sw-notice-pause").click(function () {
+    
+    sw_notice.autoplay.stop();
+  });
+  // 자동 재생 실행
+  $(".sw-notice-play").click(function () {
+   
+    sw_notice.autoplay.start();
+  });
   // ====================================
 });
